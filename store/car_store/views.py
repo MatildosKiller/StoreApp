9c11f15dc38c64d44213ce121fc4ad2091ea7ec2
MatildosKiller <lsnkandrey@outlook.com>
2025-07-29 21:19:22 +0500

    отработали представления, первые маршруты

--- store/car_store/views.py
+++ store/car_store/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    host = request.META["HTTP_HOST"] # получаем адрес сервера
+    user_agent = request.META["HTTP_USER_AGENT"]    # получаем данные бразера
+    path = request.path     # получаем запрошенный путь
+     
+    return HttpResponse(f"""
+        <p>Host: {host}</p>
+        <p>Path: {path}</p>
+        <p>User-agent: {user_agent}</p>
+    """)
+
+
+def about(request):
+    return HttpResponse("О сайте")
+ 
+def contact(request):
+    return HttpResponse("Контакты")
+
+def buy(request):
+    return
+
--- store/store/urls.py
+++ store/store/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from car_store import views
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
+    path('about', views.about),
+    path('contact', views.contact),
 ]
